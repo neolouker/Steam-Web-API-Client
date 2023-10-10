7f307f1d8116940edd3592f1f206a89f6fa73e7b
Neolouker <neolouker@gmail.com>
2023-10-10 22:33:51 +0200

    feat: scrollbar on response window

--- Steam-Web-API-Client/gui/user_interface.py
+++ Steam-Web-API-Client/gui/user_interface.py
@@ -11,21 +11,25 @@ class UserInterface:
         # Root Window
         self.root = tk.Tk()
         self.root.title("Steam Web API")
+
         # Stretch and Resize
         self.root.resizable(True, True)
         tk.Grid.rowconfigure(self.root, 0, weight=1)
         tk.Grid.columnconfigure(self.root, 0, weight=1)
         tk.Grid.columnconfigure(self.root, 1, weight=1)
+
         # Variables
         self.api_key = tk.StringVar()
         self.steam_id = tk.StringVar()
         self.data_path = "Steam-Web-API-Client\data\data.json"
+
         # Read from data\data.json
         self.data_handler = DataHandler(
             data_path=self.data_path, api_key=self.api_key, steam_id=self.steam_id)
         self.data = self.data_handler.read_data()
         self.api_key.set(self.data[0])
         self.steam_id.set(self.data[1])
+
         # Widgets
         self.label1 = tk.Label(self.root, text="Web API Key")
         self.label2 = tk.Label(self.root, text="Steam ID")
@@ -43,6 +47,7 @@ class UserInterface:
             self.root, textvariable=self.steam_id, width=40, justify="center")
         self.button1 = ttk.Button(self.root, text="Enter", command=lambda: [
             self.root.withdraw(), self.open_response_window()])
+
         # Grid Placement
         self.label1.grid(row=0, column=0, padx=20, pady=(10, 0))
         self.label2.grid(row=0, column=1, padx=20, pady=(10, 0))
@@ -71,36 +76,55 @@ class ResponseWindow:
         self.response = tk.Toplevel(self.root)
         self.response.title("Steam Web API")
         self.response.resizable(True, True)
+
+        # Create a canvas with a vertical scrollbar
+        canvas = tk.Canvas(self.response)
+        scrollbar = ttk.Scrollbar(self.response, orient="vertical")
+        scrollbar.pack(side="right", fill="y")
+
+        # Attach the scrollbar to the canvas
+        scrollbar.config(command=canvas.yview)
+
+        # Create a canvas with the vertical scrollbar
+        canvas = tk.Canvas(self.response, yscrollcommand=scrollbar.set)
+        canvas.pack(side="left", fill="both", expand=True)
+
+        # Create a frame inside the canvas to hold the widgets
+        frame = ttk.Frame(canvas)
+        canvas.create_window((0, 0), window=frame, anchor="nw")
+
         # Data Handler
         self.data_handler = DataHandler(
             data_path=self.data_path, api_key=self.api_key.get(), steam_id=self.steam_id.get())
+
         # Save Input Data in data\data.json
         self.data_handler.write_data()
+
         # Steam Web API
         self.steam_api = SteamAPI(api_key=self.api_key.get())
         games = self.steam_api.get_recently_played_games(
             steamid=self.steam_id.get())
         amount_games = games["response"]["total_count"]
-        # Iteration
+
         for i in range(amount_games):
             # Source Information
             self.steam_api.fetch_icons(games=games, iteration=i)
             self.steam_api.fetch_names(games=games, iteration=i)
             self.steam_api.fetch_playtime_2weeks(games=games, iteration=i)
             self.steam_api.fetch_playtime_forever(games=games, iteration=i)
+
             # Widgets
-            title_head = tk.Label(self.response, text=("Title"))
-            playtime_2weeks_head = tk.Label(
-                self.response, text="Last 2 Weeks")
-            playtime_forever_head = tk.Label(
-                self.response, text="Overall")
-            separator = ttk.Separator(self.response, orient="horizontal")
-            icon = tk.Label(self.response, image=self.steam_api.image_list[i])
-            title = tk.Label(self.response, text=self.steam_api.name_list[i])
+            title_head = tk.Label(frame, text=("Title"))
+            playtime_2weeks_head = tk.Label(frame, text="Last 2 Weeks")
+            playtime_forever_head = tk.Label(frame, text="Overall")
+            separator = ttk.Separator(frame, orient="horizontal")
+            icon = tk.Label(frame, image=self.steam_api.image_list[i])
+            title = tk.Label(frame, text=self.steam_api.name_list[i])
             playtime_2weeks = tk.Label(
-                self.response, text=self.steam_api.playtime_2weeks_list[i])
+                frame, text=self.steam_api.playtime_2weeks_list[i])
             playtime_forever = tk.Label(
-                self.response, text=self.steam_api.playtime_forever_list[i])
+                frame, text=self.steam_api.playtime_forever_list[i])
+
             # Grid Placement
             row_begin = i + 2
             title_head.grid(row=0, column=1, padx=10)
@@ -109,8 +133,23 @@ class ResponseWindow:
             separator.grid(row=1, column=0, columnspan=4, sticky="EW")
             icon.grid(row=row_begin, column=0, padx=10, pady=10)
             title.grid(row=row_begin, column=1, padx=40, pady=10)
-            playtime_2weeks.grid(row=row_begin, column=2, padx=10, pady=10)
-            playtime_forever.grid(row=row_begin, column=3, padx=10, pady=10)
+            playtime_2weeks.grid(row=row_begin, column=2, padx=20, pady=10)
+            playtime_forever.grid(row=row_begin, column=3, padx=20, pady=10)
+
+        # Update the scroll region whenever the canvas is updated
+        canvas.update_idletasks()
+        canvas.config(scrollregion=canvas.bbox("all"))
+
+        # Bind the canvas scrolling to the scrollbar
+        # Automatically adjust the canvas width based on the content
+        canvas.bind("<Configure>", lambda e: canvas.config(width=e.width))
+        canvas.bind_all("<MouseWheel>", lambda event: canvas.yview_scroll(
+            int(-1 * (event.delta / 120)), "units"))
+
+        # Automatically adjust the window size based on the content
+        self.response.update_idletasks()
+        self.response.geometry(
+            f"{frame.winfo_reqwidth()}x{min(frame.winfo_reqheight(), 500)}")
 
         # When the response window is closed, destroy the main window
         self.response.protocol("WM_DELETE_WINDOW", self.on_response_close)
